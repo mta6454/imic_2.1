4bbd922108d6adcd322e4e6fd2987c3de6d8b83a
mta6454 <107976532+mta6454@users.noreply.github.com>
2022-06-28 01:36:09 +0700

    Add files via upload

---
+++ color.dart
@@ -0,0 +1,13 @@
+import 'dart:ffi';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+var colorr = [
+  Colors.yellow,
+  Colors.blue,
+  Colors.red,
+  Colors.green,
+  Colors.purple,
+];
+Random random = Random();
